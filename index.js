862093ae0661d52439ec2618368be31c4d69d129
Erik Grimmbråten <79856585+grimmbraten@users.noreply.github.com>
2021-05-03 15:42:40 +0200

    simplify entry file

--- index.js
+++ index.js
@@ -1,69 +1,50 @@
 const request = require("request");
+const { convert } = require("./helpers");
+const { Flags } = require("./flags");
+const { present, log } = require("./outputs");
 
 const [, , bundle, ...flags] = process.argv;
 
-if (!bundle) return console.log("No bundle provided");
+if (!bundle) return console.log("no bundle provided");
 
 request(
   `https://bundlephobia.com/api/size?package=${bundle}`,
   (_, { statusCode }, body) => {
     if (statusCode !== 200) {
-      return statusCode === 500
-        ? console.info("Package not found")
-        : console.error("Something went wrong, please try again");
+      return log(
+        statusCode === 500
+          ? "package not found"
+          : "something went wrong, please try again"
+      );
     }
 
     const bundle = JSON.parse(body);
 
-    if ((flags.length === 1 && flags === "--raw") || flags === "-R") {
-      return console.log(bundle);
-    }
+    const zip = convert(bundle.gzip);
+    const regular = convert(bundle.size);
 
-    const {
-      name,
-      version,
-      size,
-      gzip,
-      repository,
-      description,
-      dependencyCount,
-      dependencySizes,
-      peerDependencies,
-    } = bundle;
-
-    const zip = convert(gzip);
-    const regular = convert(size);
-
-    console.log(
-      `${name}@${version}\n${
-        regular.mb >= 1 ? regular.mb + " MB" : regular.kb + " kB"
-      } / ${zip.mb >= 1 ? zip.mb + " MB" : zip.kb + " kB"}`
-    );
+    present(regular, zip, bundle.name, bundle.version);
 
     flags.forEach(flag => {
-      if (flag === "--source" || flag === "-S")
-        console.log(`\n${repository}\n${description}`);
-      else if (flag === "--dependencies" || flag === "-D") {
-        if (dependencyCount === 0) console.log("No dependencies");
-        else {
-          console.log("");
-          dependencySizes.forEach(dependency => {
-            const size = convert(dependency.approximateSize);
-            console.log(
-              `${dependency.name}: ${
-                size.mb >= 1 ? size.mb + " MB" : size.kb + " kB"
-              }`
-            );
-          });
-        }
-      } else if (flag === "--peer" || flag === "-P") {
-        if (peerDependencies.length === 0) console.log("No peers");
-        else peerDependencies.forEach(peer => console.log(peer));
+      if (Flags.raw.includes(flag)) return log(bundle);
+      if (Flags.source.includes(flag))
+        return log(`${bundle.repository} - ${bundle.description}`);
+
+      if (Flags.dependencies.includes(flag)) {
+        if (bundle.dependencyCount === 0) return log("no dependencies");
+
+        bundle.dependencySizes.forEach(dependency => {
+          const size = convert(dependency.approximateSize);
+          log(
+            `${dependency.name}: ${
+              size.mb >= 1 ? size.mb + " MB" : size.kb + " kB"
+            }`
+          );
+        });
+      } else if (Flags.peer.includes(flag)) {
+        if (!bundle.peerDependencies) return log("no peers");
+        bundle.peerDependencies.forEach(peer => log(peer));
       }
     });
   }
 );
-
-const convert = size => ({ kb: calculate(size, 1), mb: calculate(size, 2) });
-
-const calculate = (value, pow) => (value / Math.pow(1024, pow)).toFixed(1);
