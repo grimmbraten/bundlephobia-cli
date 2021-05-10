eaaf841cd821894127de2ce82799ed85e9d864df
Erik Grimmbråten <erik.grimmbraten@polestar.com>
2021-05-10 21:45:01 +0200

    improve overall logic

--- index.js
+++ index.js
@@ -1,40 +1,43 @@
 const request = require("request");
+
 const { convert } = require("./helpers");
-const { Flags } = require("./flags");
+const { Flags, Commands } = require("./types");
 const { present, log } = require("./outputs");
 
-const [, , bundle, ...flags] = process.argv;
+const [, , input, ...flags] = process.argv;
 
-if (!bundle) return console.log("no bundle provided");
+if (Commands.flags.includes(input)) return console.log(Flags);
+
+if (!input || Commands.help.includes(input))
+  return log("bp <bundle-name> [--flags]");
 
 request(
-  `https://bundlephobia.com/api/size?package=${bundle}`,
+  `https://bundlephobia.com/api/size?package=${input}`,
   (_, { statusCode }, body) => {
-    if (statusCode !== 200) {
-      return log(
-        statusCode === 500
-          ? "package not found"
-          : "something went wrong, please try again"
-      );
-    }
+    if (statusCode !== 200) return log(`woops, ${input} could not be found`);
 
     const bundle = JSON.parse(body);
 
     const zip = convert(bundle.gzip);
     const regular = convert(bundle.size);
 
-    present(regular, zip, bundle.name, bundle.version);
+    flags.length === 0 && present(regular, zip, bundle.name, bundle.version);
 
     flags.forEach(flag => {
       if (Flags.raw.includes(flag)) return log(bundle);
+
       if (Flags.source.includes(flag))
         return log(`${bundle.repository} - ${bundle.description}`);
 
       if (Flags.dependencies.includes(flag)) {
-        if (bundle.dependencyCount === 0) return log("no dependencies");
+        if (bundle.dependencyCount === 0)
+          return log(`${bundle.name} has no dependencies`);
 
-        bundle.dependencySizes.forEach(dependency => {
+        bundle.dependencySizes.forEach((dependency, index) => {
+          if (index === 0) return;
+
           const size = convert(dependency.approximateSize);
+
           log(
             `${dependency.name}: ${
               size.mb >= 1 ? size.mb + " MB" : size.kb + " kB"
@@ -42,7 +45,7 @@ request(
           );
         });
       } else if (Flags.peer.includes(flag)) {
-        if (!bundle.peerDependencies) return log("no peers");
+        if (!bundle.peerDependencies) return log(`${bundle.name} has no peers`);
         bundle.peerDependencies.forEach(peer => log(peer));
       }
     });
