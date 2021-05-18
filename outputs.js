7d87cd776155eee631dba18593bedf0d99a62e37
Erik Grimmbråten <79856585+grimmbraten@users.noreply.github.com>
2021-05-18 15:21:20 +0200

    add size color for default present output

--- outputs.js
+++ outputs.js
@@ -1,11 +1,25 @@
+const colors = require("colors");
+
 const log = output => console.log(output);
 
-const present = (regular, zip, name, version) =>
+const sizeColor = (size, output) =>
+  size < 1
+    ? colors.green(output)
+    : size >= 2
+    ? colors.yellow(output)
+    : colors.red(output);
+
+const present = (regular, zip, name, version) => {
+  const regularText = regular.mb >= 1 ? regular.mb + " MB" : regular.kb + " kB";
+  const zipText = zip.mb >= 1 ? zip.mb + " MB" : zip.kb + " kB";
+
   console.log(
-    `${name}@${version}\n${
-      regular.mb >= 1 ? regular.mb + " MB" : regular.kb + " kB"
-    } / ${zip.mb >= 1 ? zip.mb + " MB" : zip.kb + " kB"}`
+    `${name}@${version}\n${sizeColor(regular.mb, regularText)} / ${sizeColor(
+      zip.mb,
+      zipText
+    )}`
   );
+};
 
 module.exports = {
   log,
