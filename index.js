80f9bdc70be57ebda434899ec5b09bca5e3b2a13
Erik Grimmbråten <79856585+grimmbraten@users.noreply.github.com>
2021-06-10 13:26:21 +0200

    implement ora spinner

--- index.js
+++ index.js
@@ -1,58 +1,107 @@
 #!/usr/bin/env node
 
+const ora = require("ora");
 const open = require("open");
+const colors = require("colors");
 const request = require("request");
 
-const { convert } = require("./helpers");
+const { convert, sizeUnit, sizeColor } = require("./helpers");
 const { Flags, Commands } = require("./types");
-const { present, log } = require("./outputs");
 
 const [, , input, ...flags] = process.argv;
 
 const browse = `https://bundlephobia.com/result?p=${input}`;
 const endpoint = `https://bundlephobia.com/api/size?package=${input}`;
 
-if (Commands.flags.includes(input)) return console.log(Flags);
+const spinner = ora(`Starting service`).start();
 
-if (!input || Commands.help.includes(input) || input.includes("-"))
-  return log("bp <bundle-name> [--flags]");
+if (Commands.flags.includes(input))
+  return spinner.info(
+    "available flags \n--browse " +
+      "open bundlephobia in your default browser".gray +
+      "\n--dependencies " +
+      "list a bundles dependencies".gray +
+      "\n--peer " +
+      "discover a bundles peer dependencies".gray +
+      "\n--raw " +
+      "print raw api response (json)".gray +
+      "\n--source " +
+      "view bundle repository information".gray
+  );
+
+if (Commands.examples.includes(input))
+  return spinner.info(
+    "bundlephobia-cli examples" +
+      "\nbp react " +
+      "view basic information about the latest react package".gray +
+      "\nbp orb --dependencies " +
+      "list all dependencies for orb".gray +
+      "\nbp colors --source --raw " +
+      "view repository information and print raw api response (json)".gray
+  );
+
+const help =
+  "\nbp <bundle> [--flags] " +
+  "search for bundle".gray +
+  "\nbp --help " +
+  "list available commands".gray +
+  "\nbp --flags " +
+  "list available flags".gray +
+  "\nbp --examples " +
+  "show usage examples".gray;
+
+if (Commands.help.includes(input))
+  return spinner.info("how to use bundlephobia-cli" + help);
+
+if (!input || input.includes("--")) return spinner.warn("invalid usage" + help);
+
+spinner.text = "Searching for bundle";
 
 request(endpoint, (_, { statusCode }, body) => {
-  if (statusCode !== 200) return log(`could not find ${input}`);
+  if (statusCode !== 200) return spinner.fail(`could not find ${input}`);
 
   const bundle = JSON.parse(body);
+
+  spinner.succeed(`${bundle.name}@${bundle.version}`);
 
   const zip = convert(bundle.gzip);
   const regular = convert(bundle.size);
 
-  flags.length === 0 && present(regular, zip, bundle.name, bundle.version);
+  flags.length === 0 &&
+    console.log(
+      `${sizeColor(regular.mb, sizeUnit(regular))} / ${sizeColor(
+        zip.mb,
+        sizeUnit(zip)
+      )}`
+    );
 
   flags.forEach(flag => {
-    if (Flags.browse.includes(flag)) return open(browse);
-
-    if (Flags.raw.includes(flag)) return log(bundle);
-
-    if (Flags.source.includes(flag))
-      return log(`${bundle.repository} - ${bundle.description}`);
-
-    if (Flags.dependencies.includes(flag)) {
+    if (Flags.browse.includes(flag)) return console.open(browse);
+    else if (Flags.raw.includes(flag)) return console.log(bundle);
+    else if (Flags.source.includes(flag))
+      return console.log(
+        "repository: " +
+          `${bundle.repository}`.gray +
+          "\ndescription: " +
+          `${bundle.description}`.gray
+      );
+    else if (Flags.dependencies.includes(flag)) {
       if (bundle.dependencyCount === 0)
-        return log(`${bundle.name} has no dependencies`);
+        return console.log("could not find any dependencies");
 
-      bundle.dependencySizes.forEach((dependency, index) => {
-        if (index === 0) return;
-
+      bundle.dependencySizes.forEach(dependency => {
         const size = convert(dependency.approximateSize);
 
-        log(
-          `${dependency.name}: ${
-            size.mb >= 1 ? size.mb + " MB" : size.kb + " kB"
-          }`
+        console.log(
+          `${dependency.name}: ${sizeColor(size.mb, sizeUnit(size))}`
         );
       });
     } else if (Flags.peer.includes(flag)) {
-      if (!bundle.peerDependencies) return log(`${bundle.name} has no peers`);
-      bundle.peerDependencies.forEach(peer => log(peer));
+      if (!bundle.peerDependencies)
+        return console.log("could not find any peers");
+      bundle.peerDependencies.forEach(peer => console.log(peer));
+    } else {
+      console.log("invalid flag");
     }
   });
 });
