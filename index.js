9b9ddba9f45e8324d446b64ecee4557d1da50c4b
Erik Grimmbråten <79856585+grimmbraten@users.noreply.github.com>
2021-06-24 12:46:01 +0200

    simplify index

--- index.js
+++ index.js
@@ -11,28 +11,25 @@ const [, , input, flag] = process.argv;
 
 const spinner = ora(`starting service`).start();
 
-if (!input || input.includes("--"))
+if (!input || input.includes("-"))
   return spinner.info(
-    "bundlephobia-cli examples" +
-      "\nbp react " +
-      "view basic information about the latest react package".gray +
+    "bundlephobia-cli" +
+      "\nbp open " +
+      "view package size of latest open version".gray +
+      "\nbp colors -i " +
+      "fetch information about the colors package".gray +
       "\nbp orb --dependencies " +
-      "list all dependencies for orb".gray +
-      "\nbp colors --info " +
-      "view repository information".gray
+      "list all dependencies of the orb package".gray
   );
 
 const command = controller(input, flag);
-
+if (!command) return spinner.fail(`failed to resolve passed flag`);
 spinner.text = command.request;
 
 const req = request(command.endpoint, (_, { statusCode }, body) => {
   clearTimeout(timeout);
-
   if (statusCode !== 200) return spinner.fail(command.failed);
-
-  const package = JSON.parse(body);
-  command.callback(spinner, package);
+  command.callback(spinner, JSON.parse(body), input);
 });
 
 const timeout = setTimeout(() => {
