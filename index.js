ec36b6e006dac70ae2d31efac886984deafd2157
Erik Grimmbråten <79856585+grimmbraten@users.noreply.github.com>
2021-07-15 11:43:38 +0200

    merge master

--- index.js
+++ index.js
@@ -9,28 +9,30 @@ const controller = require("./controller");
 
 const [, , input, flag] = process.argv;
 
-const spinner = ora(`starting service`).start();
+(() => {
+  const spinner = ora(`starting service`).start();
 
-if (!input)
-  return spinner.info(
-    "bundlephobia-cli" +
-      "\nbp open " +
-      "view package size of latest open version".gray +
-      "\nbp colors --info " +
-      "fetch information about the colors package".gray +
-      "\nbp orb --dependencies " +
-      "list all dependencies of the orb package".gray
-  );
+  if (!input)
+    return spinner.info(
+      "bundlephobia-cli" +
+        "\nbp open " +
+        "view package size of latest open version".gray +
+        "\nbp colors --info " +
+        "fetch information about the colors package".gray +
+        "\nbp orb --dependencies " +
+        "list all dependencies of the orb package".gray
+    );
 
-const command = controller(input, flag);
-if (!command) return spinner.fail(`failed to resolve passed flag`);
-spinner.text = command.request;
+  const command = controller(input, flag);
+  if (!command) return spinner.fail(`failed to resolve passed flag`);
+  spinner.text = command.request;
 
-const req = request(command.endpoint, (_, { statusCode }, body) => {
-  clearTimeout(timeout);
-  if (statusCode !== 200) return spinner.fail(command.failed);
-  command.callback(spinner, JSON.parse(body), input);
-});
+  const req = request(command.endpoint, (_, { statusCode }, body) => {
+    clearTimeout(timeout);
+    if (statusCode !== 200) return spinner.fail(command.failed);
+    command.callback(spinner, JSON.parse(body), input);
+  });
+})();
 
 const timeout = setTimeout(() => {
   req.abort();
